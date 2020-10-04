82c448d9a680db2f776abdd4fdbc9fc84250aa47
lahirulakruwan <lahirulakruwan5@gmail.com>
2020-10-04 10:21:46 +0530

    inquery options

---
+++ app/src/main/java/com/example/tcmsapp/enqueryOptions.java
@@ -0,0 +1,22 @@
+package com.example.tcmsapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+public class enqueryOptions extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.fragment_enquiry_options,container,false);
+
+        return view;
+    }
+
+}
---
+++ app/src/main/java/com/example/tcmsapp/staffMain.java
@@ -0,0 +1,19 @@
+package com.example.tcmsapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class staffMain extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.fragment_staff_main,container,false);
+
+        return view;
+    }
+}
