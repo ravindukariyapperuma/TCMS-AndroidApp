e474468424fd5392f2e4323f8be6f8b741920b43
ravindukariyapperuma <ravindu.kariyapperuma@gmail.com>
2020-10-07 20:56:58 +0530

    'enquery-style-changes'

--- app/src/main/java/com/example/tcmsapp/Broadcast_sms.java
+++ app/src/main/java/com/example/tcmsapp/Broadcast_sms.java
@@ -8,7 +8,9 @@ import androidx.fragment.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class Broadcast_sms extends Fragment {
 
@@ -18,6 +20,7 @@ public class Broadcast_sms extends Fragment {
         final View view = inflater.inflate(R.layout.fragment_broadcast_sms,container,false);
 
         ImageView broadcastsms = view.findViewById(R.id.broadcastbackbtn);
+        Button btnSend = view.findViewById(R.id.btnSend);
 
         broadcastsms.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,6 +29,16 @@ public class Broadcast_sms extends Fragment {
                 fragmentTransaction.replace(R.id.contrainer,new EnqueryList());
 
                 fragmentTransaction.commit();
+            }
+        });
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getActivity(),
+                        "Send Successfully",
+                        Toast.LENGTH_SHORT);
+                toast.show();
             }
         });
         return view;
